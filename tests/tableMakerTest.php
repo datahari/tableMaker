751131d1c85a1de163b43a89c997363ac99de5d5
datahari <info@datahari.fi>
2016-12-15 23:33:54 +0200

    converted tabs to spaces

--- tests/tableMakerTest.php
+++ tests/tableMakerTest.php
@@ -5,96 +5,96 @@ require( __DIR__ . "/../tableMaker.class.php");
 class tableMakerTestTest extends PHPUnit_Framework_TestCase
 {
 
-	public function test_it_turns_array_items_to_table_rows()
-	{
+    public function test_it_turns_array_items_to_table_rows()
+    {
 
-		$rows = [
-			"row 1",
-			"row 2",
-			"row 3"
-		];
+        $rows = [
+            "row 1",
+            "row 2",
+            "row 3"
+        ];
 
-		$table = new tableMaker();
-		$table->setRows( $rows );
+        $table = new tableMaker();
+        $table->setRows( $rows );
 
-		$actual = $table->render();
+        $actual = $table->render();
 
-		$expected = '<table>'.
-						'<tr>'.
-							'<td>row 1</td>'.
-						'</tr>'.
-						'<tr>'.
-							'<td>row 2</td>'.
-						'</tr>'.
-						'<tr>'.
-							'<td>row 3</td>'.
-						'</tr>'.
-					'</table>';
+        $expected = '<table>'.
+                        '<tr>'.
+                            '<td>row 1</td>'.
+                        '</tr>'.
+                        '<tr>'.
+                            '<td>row 2</td>'.
+                        '</tr>'.
+                        '<tr>'.
+                            '<td>row 3</td>'.
+                        '</tr>'.
+                    '</table>';
 
-		$this->assertEquals( $expected, $actual );
+        $this->assertEquals( $expected, $actual );
 
-	}
+    }
 
-	public function test_it_turns_2dimensional_array_into_rows_and_columns()
-	{
+    public function test_it_turns_2dimensional_array_into_rows_and_columns()
+    {
 
-		$rows = [
-			["row 1a", "row 1b", "row 1c"],
-			["row 2a", "row 2b", "row 2c"],
-			["row 3a", "row 3b", "row 3c"]
-		];
+        $rows = [
+            ["row 1a", "row 1b", "row 1c"],
+            ["row 2a", "row 2b", "row 2c"],
+            ["row 3a", "row 3b", "row 3c"]
+        ];
 
-		$table = new tableMaker();
-		$table->setRows( $rows );
+        $table = new tableMaker();
+        $table->setRows( $rows );
 
-		$actual = $table->render();
+        $actual = $table->render();
 
-		$expected = '<table>'.
-						'<tr>'.
-							'<td>row 1a</td>'.
-							'<td>row 1b</td>'.
-							'<td>row 1c</td>'.
-						'</tr>'.
-						'<tr>'.
-							'<td>row 2a</td>'.
-							'<td>row 2b</td>'.
-							'<td>row 2c</td>'.
-						'</tr>'.
-						'<tr>'.
-							'<td>row 3a</td>'.
-							'<td>row 3b</td>'.
-							'<td>row 3c</td>'.
-						'</tr>'.
-					'</table>';
+        $expected = '<table>'.
+                        '<tr>'.
+                            '<td>row 1a</td>'.
+                            '<td>row 1b</td>'.
+                            '<td>row 1c</td>'.
+                        '</tr>'.
+                        '<tr>'.
+                            '<td>row 2a</td>'.
+                            '<td>row 2b</td>'.
+                            '<td>row 2c</td>'.
+                        '</tr>'.
+                        '<tr>'.
+                            '<td>row 3a</td>'.
+                            '<td>row 3b</td>'.
+                            '<td>row 3c</td>'.
+                        '</tr>'.
+                    '</table>';
 
-		$this->assertEquals( $expected, $actual );
-	}
+        $this->assertEquals( $expected, $actual );
+    }
 
-	public function test_it_substitutes_special_characters_with_htmlentities()
-	{
+    public function test_it_substitutes_special_characters_with_htmlentities()
+    {
 
-		$rows = [
-			["\"test\""],
-			["\"test\"", "\"test\"", "\"test\""]
-		];
+        $rows = [
+            ["\"test\""],
+            ["\"test\"", "\"test\"", "\"test\""]
+        ];
 
-		$table = new tableMaker();
-		$table->setRows( $rows );
+        $table = new tableMaker();
+        $table->setRows( $rows );
 
-		$actual = $table->render();
+        $actual = $table->render();
 
-		$expected = '<table>'.
-						'<tr>'.
-							'<td>&quot;test&quot;</td>'.
-						'</tr>'.
-						'<tr>'.
-							'<td>&quot;test&quot;</td>'.
-							'<td>&quot;test&quot;</td>'.
-							'<td>&quot;test&quot;</td>'.
-						'</tr>'.
-					'</table>';
+        $expected = '<table>'.
+                        '<tr>'.
+                            '<td>&quot;test&quot;</td>'.
+                        '</tr>'.
+                        '<tr>'.
+                            '<td>&quot;test&quot;</td>'.
+                            '<td>&quot;test&quot;</td>'.
+                            '<td>&quot;test&quot;</td>'.
+                        '</tr>'.
+                    '</table>';
 
-		$this->assertEquals( $expected, $actual );
-	}
+        $this->assertEquals( $expected, $actual );
+    }
 
 }
